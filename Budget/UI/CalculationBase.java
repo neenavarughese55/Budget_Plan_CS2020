4a24aeab7191202e049be96a5339decda4e577b2
Neena Varughese <158041703+neenavarughese55@users.noreply.github.com>
2024-12-03 14:03:51 +0000

    created a calculation file

--- Budget/UI/CalculationBase.java
+++ Budget/UI/CalculationBase.java
@@ -1,5 +1,48 @@
 package Budget.UI;
 
-public class CalculationBase {
+import javax.swing.*;
+
+import Budget.Main.BudgetBase;
+import Budget.UI.IncomeBase;
+import Budget.UI.SpendingBase;
+
+import java.awt.*;
+
+public class CalculationBase extends JLabel {
+
+    JFrame topLevelFrame; // top-level JFrame
+    GridBagConstraints layoutConstraints = new GridBagConstraints();
+
+    private JLabel leftoverField;
+
+    public CalculationBase(JFrame frame) {
+        topLevelFrame = frame; // keep track of top-level frame
+        setLayout(new GridBagLayout()); // use GridBag layout
+        CalculationinitComponents(); // initalise components
+    }
+
+    private void CalculationinitComponents() {
+
+    }
+
+    public double IncomeMinusSpending() {
+        double totalIncome = getTextFieldValue(IncomeBase.totalIncomeField);
+        double totalSpend = getTextFieldValue(SpendingBase.totalSpendField);
+
+        if (Double.isNaN(totalIncome) || Double.isNaN(totalSpend)) {
+            SpendingBase.totalSpendField.setText(""); // clear total income field
+            totalIncome = 0.0;
+            return totalIncome; // exit method and do nothing
+        }
+
+        double leftover = totalIncome - totalSpend;
+
+        if (leftover < 0) {
+            leftoverField.setText(String.format("<html><span style='color:red;'>%.2f</span></html>", leftover));
+        } else {
+            leftoverField.setText(String.format("<html><span style='color:black;'>%.2f</span></html>", leftover));
+        }
+        return leftover;
+    }
 
 }
